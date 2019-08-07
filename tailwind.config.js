0e8abb23825b0920138b659e9f6f62844a48c12e
Justin Reidy <justintreidy@gmail.com>
2019-08-07 16:21:21 -0500

    finished sidebar navigation, began work on tweets

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {}
+  },
+  variants: {
+
+    textColor:['responsive', 'hover', 'focus', 'active'],
+    borderColor:['responsive', 'hover', 'focus', 'active'],
+    borderWidth:['responsive', 'hover', 'focus', 'active'],
+
+  },
+  plugins: []
+}
